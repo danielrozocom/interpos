d5dc5e9ff062dc434d9dcad6230822ce2b5e4df6
Daniel Rozo <danielrozov15@gmail.com>
2025-07-13 21:08:15 -0500

    Commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,7 +27,7 @@
               <path class={menuOpen ? 'block' : 'hidden'} stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
-        </div>
+
       </div>
       <!-- Mobile menu -->
       {#if menuOpen}
